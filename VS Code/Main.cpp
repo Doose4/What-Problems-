6d856027bb1af4c103949ca638f547493331c183
Doose4 <157078178+Doose4@users.noreply.github.com>
2024-01-30 20:35:07 -0600

    Update code
    Updating code after some time

--- VS Code/Main.cpp
+++ VS Code/Main.cpp
@@ -2,46 +2,30 @@
 #include <string>
 #include <cctype> 
 
+// do an array?
+
 using namespace std;
 
-void question(int n) {
-	cout << "num";
-
-}
-
-void question(char n) {
-
-	cout << "char";
-
-}
-
 int main() {
-	string s = "";
-	int n, i;
+	string s = "", numbe;
+	int n, i, lastnum;
 	char c;
 
 	cout << "Please enter the assignment: ";
-	//s = cin.peek();
 	cin >> s;
-	cout << "\n";
-	//cout << cin.peek();
-	cout << s.length();
-	cout << s[1];
 
 	for (i=0; i < s.length(); i++) {
 		if (isdigit(s[i])){
 			n = static_cast<int>(s[i]);
-			cout << "You entered the number: " << n << '\n';
-			//question(n);
-		}
-		else
-		{
+			//cout << "You entered the number: " << n << '\n';
+			lastnum = n;
+		} else {
 			c = static_cast<char>(s[i]);
-			cout << "You entered the word: " << c << '\n';
-			//question(c);
 		}
 
 	}
+
+	cout << "Do problems " << numbe << " and " << lastnum << " of " << c;
 
 	return 0;
 }
