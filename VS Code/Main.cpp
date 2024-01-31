97856cfbd7dc431aede2ffc5f1bbdaca66f8f809
Doose4 <157078178+Doose4@users.noreply.github.com>
2024-01-30 20:49:56 -0600

    Adding code

--- VS Code/Main.cpp
+++ VS Code/Main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <cctype> 
 
 // do an array?
@@ -7,25 +8,46 @@
 using namespace std;
 
 int main() {
-	string s = "", numbe;
+	vector<int> numbe;
+	string s = "", nums = "";
 	int n, i, lastnum;
 	char c;
 
 	cout << "Please enter the assignment: ";
 	cin >> s;
 
-	for (i=0; i < s.length(); i++) {
+
+
+	for (i=0; i < s.length()-1; i++) {
 		if (isdigit(s[i])){
 			n = static_cast<int>(s[i]);
 			//cout << "You entered the number: " << n << '\n';
-			lastnum = n;
+			numbe.push_back(n);
 		} else {
 			c = static_cast<char>(s[i]);
+			switch (c) {
+			case '-':
+				for (int i = n + 1; i <= static_cast<int>(s[i + 1]); i++) {
+					numbe.push_back(n);
+				}
+				break;
+			case ',':
+				break;
+			default:
+				char chap = c;
+				break;
+			}
+			
 		}
 
 	}
 
-	cout << "Do problems " << numbe << " and " << lastnum << " of " << c;
+	for (auto k = numbe.begin(); k < numbe.end(); k++) {
+		nums = nums + static_cast<char>(*k) + ", ";
+	}
+		nums = nums + " and " + static_cast<char>(n);
+
+	cout << "Do problems " << nums << " of " << c;
 
 	return 0;
 }
