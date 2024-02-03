a76bceff11b6f85928549024e0a6f60e808bbfad
Doose4 <157078178+Doose4@users.noreply.github.com>
2024-02-03 00:05:26 -0600

    Update Code

--- VS Code/Main.cpp
+++ VS Code/Main.cpp
@@ -2,52 +2,133 @@
 #include <string>
 #include <vector>
 #include <cctype> 
-
-// do an array?
-
+#include <sstream>
+#include <algorithm>
 using namespace std;
 
+vector<int> Vec1{}, Vec2{}, Vec3{}; // Vectooooorrrrssss
+
 int main() {
-	vector<int> numbe;
-	string s = "", nums = "";
-	int n, i, lastnum;
-	char c;
+	string s = "", chap = "";
+	int i = 0;
+	bool ky = 0, ky2 = 0;
 
 	cout << "Please enter the assignment: ";
-	cin >> s;
+	getline(cin, s);
 
-
-
-	for (i=0; i < s.length()-1; i++) {
-		if (isdigit(s[i])){
-			n = static_cast<int>(s[i]);
-			//cout << "You entered the number: " << n << '\n';
-			numbe.push_back(n);
-		} else {
-			c = static_cast<char>(s[i]);
+	for (char& c : s) {
+		if (!isdigit(c)) {
 			switch (c) {
 			case '-':
-				for (int i = n + 1; i <= static_cast<int>(s[i + 1]); i++) {
-					numbe.push_back(n);
-				}
+				ky = 1;
+				c = ' ';
 				break;
 			case ',':
+				i = i++;
+				c = ' ';
+				break;
+			case ' ':
+				c = ' ';
 				break;
 			default:
-				char chap = c;
+				chap = c;
+				c = ' ';
 				break;
 			}
+			ky2 = 0;
+		}
+		else {
+			if (ky2 == 0) {
+				if (ky == 1) {
+					Vec1.push_back(i);
+					ky = 0;
+					ky2 = 1;
+				}
+				else {
+					Vec1.push_back(i);
+				}
+				ky2 = 1;
+			}
+			else {
+				ky2 = 0;
+			}
+
+
+		}
+	}
+
+	int v;
+	stringstream ss(s);
+	while (ss >> v) Vec2.push_back(v);;
+
+	for (int t = 0; t < Vec1.size(); t++)
+	{
+		cout << Vec1[t];
+	}
+	cout << endl;
+
+	for (int t = 0; t < Vec2.size(); t++)
+	{
+		cout << Vec2[t];
+	}
+	cout << endl;
+
+	cout << "The problems are: ";
+	int k = 0;
+
+	switch (Vec2.size()) {
+	case 0:
+		cout << "\nYou did not enter a proper input...\n";
+		return 0;
+		break;
+	case 1:
+		Vec3.push_back(Vec2[0]);
+		break;
+	case 2:
+		if (Vec1[k] == Vec1[int(k + 1)]) {
+			for (int j = Vec2[k]; j <= Vec2[int(int(k + 1))]; j++) {
+				Vec3.push_back(j);
+			}
+			sort(Vec3.begin(), Vec3.end());
+			Vec3.erase(unique(Vec3.begin(), Vec3.end()), Vec3.end());
+			for (k = 0; k < Vec3.size() - 1; k++) {
+				cout << Vec3[k] << ", ";
+			}
+			
 			
 		}
-
+		else {
+			Vec3.push_back(Vec2[k]);
+			Vec3.push_back(Vec2[k + 1]);
+			sort(Vec3.begin(), Vec3.end());
+			Vec3.erase(unique(Vec3.begin(), Vec3.end()), Vec3.end());
+			for (k = 0; k < Vec3.size() - 1; k++) {
+				cout << Vec3[k] << ", ";
+			}
+		}
+		break;
+	default:
+		for (k = 1; k < Vec2.size(); k++) {
+			if (Vec1[int(k - 1)] == Vec1[int(k)]) {
+				for (int j = Vec2[k - 1]; j < Vec2[int(k)]; j++) {
+					Vec3.push_back(j);
+				}
+				Vec3.push_back(Vec2[k]);
+				k = k++;
+			}
+			else {
+				Vec3.push_back(Vec2[k - 1]);
+			}
+		}
+		k = k--;
+		sort(Vec3.begin(), Vec3.end());
+		Vec3.erase(unique(Vec3.begin(), Vec3.end()), Vec3.end());
+		for (k = 0; k < Vec3.size() - 1; k++) {
+			cout << Vec3[k] << ", ";
+		}
+		break;
 	}
 
-	for (auto k = numbe.begin(); k < numbe.end(); k++) {
-		nums = nums + static_cast<char>(*k) + ", ";
-	}
-		nums = nums + " and " + static_cast<char>(n);
-
-	cout << "Do problems " << nums << " of " << c;
-
+	cout << Vec3[k] << " of " << chap;
 	return 0;
 }
